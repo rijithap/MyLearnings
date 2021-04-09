2b7d838ceb7d27b2ef305188c28625d473fa5d73
rijithap <82220196+rijithap@users.noreply.github.com>
2021-04-09 19:24:02 +0530

    Create HeadAndTailMinCount.cpp
    First commit

---
+++ HeadAndTailMinCount/src/HeadAndTailMinCount.cpp
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    vector<int> A{1,1,0,1,1};
+    int numOf1s = 0;
+    int numOf0s = 0;
+    int numOfAttempts = 0;
+    int numOf1sXored0 = 0;
+    int numOf1sXored1 = 0;
+
+    cout<<"A :"; 
+    for (int j : A)
+    {
+        cout << j << " ";
+        if(j==1)
+            numOf1s++;
+        else
+            numOf0s++;
+
+    }
+
+        
+    cout<<"\nA size :"<<A.size()<<"\n";
+
+    vector<int> A_Expected0;
+    vector<int> A_Expected1;
+    vector<int> A_XORED0;
+    vector<int> A_XORED1;
+    
+    for(int i=0;i<A.size();i++)
+    {
+        if(i%2==0)
+        {
+            A_Expected0.push_back(0);
+        }
+        else
+        {
+            A_Expected0.push_back(1);
+        }
+        
+    }
+
+    cout<<"A_Expected0 :"; 
+    for (int x : A_Expected0)
+        cout << x << " ";
+        
+    cout << "\n";
+    
+    for(int i=0;i<A.size();i++)
+    {
+        if(i%2==0)
+        {
+            A_Expected1.push_back(1);
+        }
+        else
+        {
+            A_Expected1.push_back(0);
+        }
+        
+    }
+    
+    cout<<"A_Expected1 :"; 
+    for (int y : A_Expected1)
+        cout << y << " ";
+        
+ 
+    
+    for(int i=0;i<A.size();i++)
+    {
+        A_XORED0.push_back(A[i]^A_Expected0[i]);
+        A_XORED1.push_back(A[i]^A_Expected1[i]);      
+    }
+    cout << "\n";
+    
+    cout<<"A_XORED0 :"; 
+    for (int k : A_XORED0)
+    {
+        cout << k << " ";
+        if(k==1)
+            numOf1sXored0++;
+
+    }
+
+    
+    cout << "\n";    
+    
+    cout<<"A_XORED1 :";     
+    for (int l : A_XORED1)
+    {
+        cout << l << " ";
+        if(l==1)
+            numOf1sXored1++;
+
+    }
+    
+    cout << "\n"; 
+    numOfAttempts = numOf1sXored0 > numOf1sXored1 ? numOf1sXored1 : numOf1sXored0;
+    
+    cout <<"Minimum attempts needed to sort : " << numOfAttempts ;
+    return numOfAttempts;
+}
